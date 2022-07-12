d4651f923ebaffb6bb0378e6febbc249773d7a38
arafat-alim <arafat97alim@gmail.com>
2022-07-13 01:20:15 +0530

    successfully create a counter application using Redux toolkit.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { actions } from "./Store/index";
 
 function App() {
   const selector = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const handleInc = () => {
-    dispatch({ type: "INC" });
+    // dispatch({ type: "INC" });
+    dispatch(actions.increment());
   };
   const handledec = () => {
-    dispatch({ type: "DEC" });
+    // dispatch({ type: "DEC" });
+    dispatch(actions.decrement());
   };
 
   const addValue = () => {
-    dispatch({ type: "ADD", payload: 10 });
+    // dispatch({ type: "ADD", payload: 10 });
+    dispatch(actions.addByValue(10));
   };
   return (
     <div>
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,25 +1,48 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-//! creating reducers function
-const reducersFn = (state = { counter: 0 }, action) => {
-  //funciton tp  update the state
-  if (action.type === "INC") {
-    return { counter: state.counter + 1 };
-  }
-  if (action.type === "DEC") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-  //   getting a payload
-  if (action.type === "ADD") {
-    return {
-      counter: state.counter + action.payload,
-    };
-  }
+// //! creating reducers function
+// const reducersFn = (state = { counter: 0 }, action) => {
+//   //funciton tp  update the state
+//   if (action.type === "INC") {
+//     return { counter: state.counter + 1 };
+//   }
+//   if (action.type === "DEC") {
+//     return {
+//       counter: state.counter - 1,
+//     };
+//   }
+//   //   getting a payload
+//   if (action.type === "ADD") {
+//     return {
+//       counter: state.counter + action.payload,
+//     };
+//   }
 
-  return state;
-};
+//   return state;
+// };
 
-const store = createStore(reducersFn);
+// const store = createStore(reducersFn);
+// export default store;
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state, action) {
+      state.counter++;
+    },
+    decrement(state, action) {
+      state.counter--;
+    },
+    addByValue(state, action) {
+      state.counter += action.payload;
+    },
+  },
+});
+// creating an action
+export const actions = counterSlice.actions;
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 export default store;
