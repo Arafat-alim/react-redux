45597b021da0d81f88afd8c9d5f15aaa8edd4919
arafat-alim <arafat97alim@gmail.com>
2022-07-13 00:16:01 +0530

    basic counter application is built.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
 function App() {
   const selector = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const handleInc = () => {
+    dispatch({ type: "INC" });
+  };
+  const handledec = () => {
+    dispatch({ type: "DEC" });
+  };
+
+  const addValue = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  };
   return (
     <div>
       <h1>Counter</h1>
       <h3>{selector}</h3>
+      <button onClick={handleInc}>Increment</button>
+      <button onClick={handledec}>Increment</button>
+      <button onClick={addValue}>Add Value by 10</button>
     </div>
   );
 }
--- src/Store/index.js
+++ src/Store/index.js
@@ -2,6 +2,22 @@ import { createStore } from "redux";
 
 //! creating reducers function
 const reducersFn = (state = { counter: 0 }, action) => {
+  //funciton tp  update the state
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "DEC") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  //   getting a payload
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + action.payload,
+    };
+  }
+
   return state;
 };
 
